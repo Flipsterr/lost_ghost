0520ec2c4a3d30efb23840380c31ac72d4868ab5
Flipsterr <filip.von.bromssen@gmail.com>
2021-03-07 21:41:02 +0100

    added character

---
+++ README.md
@@ -0,0 +1,9 @@
+## Welcome to project Lost_ghost (still working on the name)
+
+### This is the story of Mr DapperGhost, a ghost who wandered into a new world and need to find a way to get home.
+
+#### Use the WASD keys to move around!
+
+#### The game is in it's early stage(like really eaaaaaaarly stage). Enjoy this soon to be 2d platformer!
+
+##### /Filip
--- main.lua
+++ main.lua
@@ -1,34 +1,69 @@
-rectangle = {
-    ["x"] = 300,
-    ["y"] = 400,
-    ["width"] = 20,
-    ["height"] = 20,
-    ["velocity"] = 10
+
+
+dapperMan = {
+   
+
 }
 
-gravity = 0.4
+gravity = 1100
 
 function love.load()
-    
+    love.graphics.setDefaultFilter("nearest", "nearest")
+    dapperMan.texture = love.graphics.newImage("lost_ghost_dapper.png")
+    dapperMan.position = {
+        ["X"] = 200,
+        ["Y"] = 300
+    }
+    dapperMan.width = 16
+    dapperMan.height = 16
+    dapperMan.velocity = {
+        ["X"] = 0,
+        ["Y"] = 0
+    }
+    dapperMan.onGround = false
+    dapperMan.quad = love.graphics.newQuad(0, 0, dapperMan.width, dapperMan.height, dapperMan.texture:getWidth(), dapperMan.texture:getHeight())
 end
 
-function love.update()
+function love.update(dt)
+    dapperMan.velocity.X = 0
     if love.keyboard.isDown("d") then 
-        rectangle.x = rectangle.x + 10
+        dapperMan.velocity.X = 100
     end
     if love.keyboard.isDown("a") then 
-        rectangle.x = rectangle.x - 10
+        dapperMan.velocity.X = -100
     end
-    if love.keyboard.isDown("w") then 
-        rectangle.y = rectangle.y - 10
+    if love.keyboard.isDown("w") and dapperMan.onGround == true then 
+        dapperMan.velocity.Y = -500
+        dapperMan.onGround = false
     end
-    if rectangle.y < 400 then 
-        rectangle.velocity = gravity + rectangle.velocity
-        rectangle.y = rectangle.velocity
+    dapperMan.velocity.Y = dapperMan.velocity.Y + gravity * dt
+
+    dapperMan.position.X = dapperMan.position.X + dapperMan.velocity.X * dt
+    dapperMan.position.Y = dapperMan.position.Y + dapperMan.velocity.Y * dt
+
+    dapperMan.position.Y = math.min(dapperMan.position.Y, 200)
+
+    if dapperMan.position.Y == 200 then 
+        dapperMan.onGround = true
     end
 
 end
 
 function love.draw (dt)
-    love.graphics.rectangle( "fill", rectangle.x, rectangle.y, rectangle.width, rectangle.height)
+love.graphics.scale(2, 2)
+    love.graphics.draw(
+        dapperMan.texture,
+        dapperMan.quad,
+        dapperMan.position.X,
+        dapperMan.position.Y,
+        0,
+        1,
+        1,
+        dapperMan.width / 2,
+        dapperMan.height
+    )
+    love.graphics.setColor(1, 1, 1, 1)
+    love.graphics.line(0, 200, love.graphics.getWidth(), 200)
+    love.graphics.setLineWidth(0.2)
 end
+
